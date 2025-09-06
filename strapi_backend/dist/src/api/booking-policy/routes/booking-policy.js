005801b610c7249e0bcf253bb266c1f4da95d4e2
kazeric <kahindie873@gmail.com>
2025-09-06 03:15:22 +0300

    oops

---
+++ strapi_backend/dist/src/api/booking-policy/routes/booking-policy.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * booking-policy router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::booking-policy.booking-policy');
